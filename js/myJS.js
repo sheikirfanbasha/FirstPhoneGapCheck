7c632d2b20493c91adf31ab753e878e8dc797430
irfan basha <sheikirfanbasha@gmail.com>
2015-05-28 18:37:20 +0530

    first commit

---
+++ js/myJS.js
@@ -0,0 +1,8 @@
+function perform(){
+	var myDiv = document.body.querySelector('#result');
+	myDiv.innerHTML += "<h1>Hello world</h1><br>";
+}
+function clearTex(){
+var myDiv = document.body.querySelector('#result');
+	myDiv.innerHTML = "";
+}
